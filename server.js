dcbc7003aa299582a168933597176a59f48b1012
Ramik <ramil.sadj@mail.ru>
2018-06-22 16:58:05 +0500

    test

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+const port = 3000
+
+app.use(bodyParser.json())
+app.get('/', function(request, response) {
+    response.send('hello');
+})
+app.post('/github', function(request, response){
+    console.log(request.body);
+    //response.send(request);
+});    
+
+app.listen(port, (err) => {
+if (err) {
+return console.log('something bad happened', err)
+}
+
+console.log(`server is listening on ${port}`)
+})
